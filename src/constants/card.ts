6a373ca27ae41ccf9b287fb2c2e7ba397ab93e7d
Kadphol <kadphol.len@outlook.co.th>
2025-01-03 01:00:36 +0700

    refactor(card): refactor card type

--- src/constants/card.ts
+++ src/constants/card.ts
@@ -1,11 +1,13 @@
-export const SUIT = {
+import { RankType, SuitType } from '@/types/card'
+
+export const SUIT: Record<string, SuitType> = {
   HEARTS: 'hearts',
   DIAMONDS: 'diamonds',
   CLUBS: 'clubs',
   SPADES: 'spades',
 }
 
-export const RANK = {
+export const RANK: Record<string, RankType> = {
   TWO: '2',
   THREE: '3',
   FOUR: '4',
@@ -21,8 +23,8 @@ export const RANK = {
   ACE: 'A',
 }
 
-export const SUITS = [SUIT.HEARTS, SUIT.DIAMONDS, SUIT.CLUBS, SUIT.SPADES]
-export const RANKS = [
+export const SUITS: SuitType[] = [SUIT.HEARTS, SUIT.DIAMONDS, SUIT.CLUBS, SUIT.SPADES]
+export const RANKS: RankType[] = [
   RANK.TWO,
   RANK.THREE,
   RANK.FOUR,
